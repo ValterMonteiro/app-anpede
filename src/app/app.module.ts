9127043dd4b27534463754fc15ff17f00fcfabb6
ValterMonteiro <valtinms@gmail.com>
2023-10-30 22:38:57 -0400

    Configuracoes Globais - config, models, service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { EquipamentoService } from './services/domain/equipamento.service';
+import { AssociadoService } from './services/domain/associado.service';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    EquipamentoService, AssociadoService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
