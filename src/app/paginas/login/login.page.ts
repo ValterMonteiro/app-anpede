c937199200fc7e2d80588b65811e2d841e2ac374
ValterMonteiro <valtinms@gmail.com>
2023-10-30 22:49:34 -0400

    Login atualizando projeto

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -8,23 +8,25 @@ import { MenuController, NavController } from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
 
-  constructor(public nav: NavController, public menu: MenuController) { }
+  constructor(public nav: NavController,
+    public menu: MenuController) { }
 
-  logar() {
+  ionViewWillEnter(){
+    this.menu.enable(false);
+  }
+
+  ionViewDidLeave(){
+    this.menu.enable(true);
+  }
+
+  logar(){
     this.nav.navigateForward('home');
   }
-  cadastrar() {
+
+  registrar(){
     this.nav.navigateForward('add-associado');
   }
 
-  //Quando entrar na página de Login
-  ionViewWillEnter() {
-    this.menu.enable(false);
-  }
-  //Quando sair da página de Login
-  ionViewDidLeave() {
-    this.menu.enable(true);
-  }
 
   ngOnInit() {
   }
